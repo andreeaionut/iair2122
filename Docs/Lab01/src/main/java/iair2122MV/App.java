16004d5f0e90a11eddfb74235f309d652181451a
Andreea Ionut <andreea.ionut98@yahoo.com>
2019-03-07 12:42:07 +0200

    Initial commit

---
+++ Docs/Lab01/src/main/java/iair2122MV/App.java
@@ -0,0 +1,13 @@
+package iair2122MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
